06eb2e510f17a8455c925934e6ea1912e4914c64
Gabriel De Araujo <123610934+Gabe3231@users.noreply.github.com>
2024-05-23 12:57:33 -0400

    Add files via upload

---
+++ basicCalculator2.h
@@ -0,0 +1,19 @@
+#ifndef BASICCALCULATOR2_H
+#define BASICCALCULATOR2_H
+
+#include <iostream>
+
+class calculator
+{
+public:	
+	void addition(int value, int value2, int result);
+	void subtraction(int value, int value2, int result);
+	void division(int value, int value2, int result);
+	void multiplcation(int value, int value2, int result);
+	
+};
+
+
+
+#endif
+
